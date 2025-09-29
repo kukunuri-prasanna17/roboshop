91ce3a09fe53c03018a6006f687fb0e2264d9b3b
Kukunuri Prasanna <prasannakukunuri35@gmail.com>
2025-09-29 12:58:10 +0530

    session-06

--- roboshop.sh
+++ roboshop.sh
@@ -20,6 +20,7 @@ else
     IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID 
     --query 'Reservations[0].Instances[0].PublicIpAddress'
      --output text)
+fi
 
        echo "$instance=$IP"
 done
