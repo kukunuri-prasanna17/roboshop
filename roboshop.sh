2a204a17ac798cc941000280deecb3fe254b520d
Kukunuri Prasanna <prasannakukunuri35@gmail.com>
2025-09-29 13:52:30 +0530

    session-06

--- roboshop.sh
+++ roboshop.sh
@@ -3,9 +3,9 @@
 AIM_ID="ami-09c813fb71547fc4f"
 SG_ID="sg-036990b2c79305f16"
 
-for instances in $@
+for instance in $@
 do
-     INSTANCE_ID=$(aws ec2 run-instances --image-id $AIM_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]"--query 'Instances[0].InstanceId' --output text)
+     INSTANCE_ID=$(aws ec2 run-instances --image-id $AIM_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
 
     if [ $instances != "frontend" ]; then
          IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
