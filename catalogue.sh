a9e2038ce6e6bf5f2b102974951c8e4361c5f36c
srilakshmicloud <prasannakukunuri53@gmail.com>
2025-10-05 09:05:37 +0530

    roboshop

--- catalogue.sh
+++ catalogue.sh
@@ -9,7 +9,7 @@ N="\e[0m"
 LOGS_FOLDER="/var/log/shell-roboshop"
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
 SCRIPT_DIR=$PWD
-MONGODB_HOST=mongodb.daws96s.cfd
+MONGODB_HOST=mongodb.daws86s.cfd
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 # /var/log/shell-roboshop/catalogue.log
 
